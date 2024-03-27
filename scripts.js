adbf3dc5a6b7e16b8dfc6129cb8e8eb73f1a959f
yoelferreyra <yoelferreyra24@gmail.com>
2024-03-27 10:44:16 -0300

    Adding skills section

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
 
 let typed = new Typed('.typing', {
-    strings: ['Web Developer', 'Web Designer', 'Freelancer'],
+    strings: ['Web Developer', 'Full Stack Developer', 'Freelancer'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
