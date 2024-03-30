a68c1610719b95069623e70701a03541d635bf98
yoelferreyra <yoelferreyra24@gmail.com>
2024-03-30 19:18:30 -0300

    Adding animations and responsive to the portfolio

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
 
-
+/*===================== Typing Stack =====================*/
 let typed = new Typed('.typing', {
     strings: ['Web Developer', 'Full Stack Developer', 'Freelancer'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
+
+/*===================== Scroll reveal =====================*/
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
